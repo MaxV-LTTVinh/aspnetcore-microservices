7bf67246fdb48b18a1c31c6ce733e1d48f9a7beb
Vinh.LeTranThe <thevinh.it.kh.1997@gmail.com>
2022-09-14 17:30:34 +0700

    2.1

---
+++ aspnetcore-microservices/src/Services/Product.API/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,29 @@
+//using HealthChecks.UI.Client;
+//using Infrastructure.Middlewares;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
+namespace Product.API.Extensions;
+
+public static class ApplicationExtensions
+{
+    public static void UseInfrastructure(this IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API"));
+        //app.UseMiddleware<ErrorWrappingMiddleware>();
+        // app.UseAuthentication();
+        app.UseRouting();
+        // app.UseHttpsRedirection(); //for production only
+        // app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            //endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+            //{
+            //    Predicate = _ => true,
+            //    //ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            //});
+            endpoints.MapDefaultControllerRoute();
+        });
+    }
+}
